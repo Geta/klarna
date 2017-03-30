644bc89355baeeed3b05a2114533f8562862673b
Patrick van Kleef <patrick@geta.no>
2017-03-30 12:57:17 +0200

    Create session

--- src/Klarna.Payments/KlarnaService.cs
+++ src/Klarna.Payments/KlarnaService.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using EPiServer.Commerce.Order;
+using EPiServer.Globalization;
 using EPiServer.ServiceLocation;
 using Klarna.Payments.Models;
+using Mediachase.Commerce;
 
 namespace Klarna.Payments
 {
@@ -18,18 +22,27 @@ namespace Klarna.Payments
             _orderGroupTotalsCalculator = orderGroupTotalsCalculator;
         }
 
+        public async Task<string> CreateSession(CreateSessionRequest request)
+        {
+            var response = await _klarnaServiceApi.CreatNewSession(request).ConfigureAwait(false);
+
+            return response.ClientToken;
+        }
+
         public async Task<string> CreateSession(ICart cart)
         {
             var request = new CreateSessionRequest();
             
             var totals = _orderGroupTotalsCalculator.GetTotals(cart);
 
-            request.OrderAmount = (int) totals.Total.Amount;
-            request.OrderTaxAmount = (int)totals.TaxTotal.Amount;
+            var shipment = cart.GetFirstShipment();
+
+            request.OrderAmount = GetAmount(totals.SubTotal);
+           // request.OrderTaxAmount = GetAmount(totals.TaxTotal);
 
             request.PurchaseCurrency = cart.Currency.CurrencyCode;
-            request.PurchaseCountry = "US";
-            request.Locale = "en-us";
+            request.PurchaseCountry = GetTwoLetterCountryCode(shipment.ShippingAddress.CountryCode);
+            request.Locale = ContentLanguage.PreferredCulture.Name;
 
             var list = new List<OrderLines>();
             foreach (var item in cart.GetAllLineItems())
@@ -38,8 +51,10 @@ namespace Klarna.Payments
                 orderLine.Quantity = (int)item.Quantity;
                 orderLine.Name = item.DisplayName;
                 orderLine.Reference = item.Code;
-                orderLine.TotalAmount = orderLine.UnitPrice = (int) item.PlacedPrice;
-                
+                orderLine.UnitPrice = GetAmount(item.PlacedPrice);
+                orderLine.TotalDiscountAmount = orderLine.UnitPrice - GetAmount(item.GetDiscountedPrice(cart.Currency));
+                orderLine.TotalAmount = orderLine.UnitPrice - orderLine.TotalDiscountAmount;
+
                 list.Add(orderLine);
             }
             request.OrderLines = list.ToArray();
@@ -57,6 +72,29 @@ namespace Klarna.Payments
         public WidgetColorOptions GetWidgetColorOptions()
         {
             return new WidgetColorOptions();
+        }
+
+        private int GetAmount(decimal money)
+        {
+            if (money > 0)
+            {
+                return (int)(money * 100);
+            }
+            return 0;
+        }
+
+        private int GetAmount(Money money)
+        {
+            if (money.Amount > 0)
+            {
+                return (int)(money.Amount * 100);
+            }
+            return 0;
+        }
+
+        private string GetTwoLetterCountryCode(string code)
+        {
+            return ISO3166.Country.List.FirstOrDefault(x => x.ThreeLetterCode.Equals(code, StringComparison.InvariantCultureIgnoreCase))?.TwoLetterCode;
         }
     }
 }
