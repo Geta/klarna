69b25d3727ff6c8cbc530290fab6ce9febacec64
Patrick van Kleef <patrick@geta.no>
2017-04-20 15:08:25 +0200

    Refactor KlarnaPaymentController

--- src/demo/Sources/EPiServer.Reference.Commerce.Site/Features/Checkout/Controllers/KlarnaPaymentController.cs
+++ src/demo/Sources/EPiServer.Reference.Commerce.Site/Features/Checkout/Controllers/KlarnaPaymentController.cs
@@ -1,7 +1,5 @@
 ﻿using System.Threading.Tasks;
 using System.Web.Http;
-using EPiServer.Reference.Commerce.Site.Features.Cart.Services;
-using EPiServer.ServiceLocation;
 using Klarna.Payments;
 using EPiServer.Logging;
 using Klarna.Payments.Models;
@@ -13,28 +11,18 @@ namespace EPiServer.Reference.Commerce.Site.Features.Checkout.Controllers
     public class KlarnaPaymentController : ApiController
     {
         private ILogger _log = LogManager.GetLogger(typeof(KlarnaPaymentController));
+        private readonly IKlarnaService _klarnaService;
+
+        public KlarnaPaymentController(IKlarnaService klarnaService)
+        {
+            _klarnaService = klarnaService;
+        }
 
         [Route("session/{sessionId}")]
         [AcceptVerbs("GET")]
         public async Task<IHttpActionResult> GetSession(string sessionId)
         {
-            var klarnaService = ServiceLocator.Current.GetInstance<IKlarnaService>();
-
-            var result = await klarnaService.GetSession(sessionId);
-
-            return Ok(result);
-        }
-
-        [Route("authorization")]
-        [AcceptVerbs("GET")]
-        public async Task<IHttpActionResult> GetAuthorization()
-        {
-            var klarnaService = ServiceLocator.Current.GetInstance<IKlarnaService>();
-            var cartService = ServiceLocator.Current.GetInstance<ICartService>();
-
-            var cart = cartService.LoadCart(cartService.DefaultCartName);
-            
-            var result = await klarnaService.GetAuthorizationModel(klarnaService.GetSessionId(cart));
+            var result = await _klarnaService.GetSession(sessionId);
 
             return Ok(result);
         }
@@ -52,9 +40,7 @@ namespace EPiServer.Reference.Commerce.Site.Features.Checkout.Controllers
             {
                 var notification = JsonConvert.DeserializeObject<NotificationModel>(requestParams);
 
-                var klarnaService = ServiceLocator.Current.GetInstance<IKlarnaService>();
-
-                klarnaService.FraudUpdate(notification);
+                _klarnaService.FraudUpdate(notification);
             }
             return Ok();
         }
