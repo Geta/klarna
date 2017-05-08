7403515fc13713e0b0a8654d0a23a09175ca9f16
Brian Weeteling <brianweeteling@gmail.com>
2017-05-08 11:31:40 +0200

    Add nuget packages

--- src/pack.bat
+++ src/pack.bat
@@ -10,6 +10,8 @@ nuget pack Klarna.Payments\Klarna.Payments.csproj -IncludeReferencedProjects
 nuget pack Klarna.Payments.CommerceManager\Klarna.Payments.CommerceManager.csproj -IncludeReferencedProjects
 nuget pack Klarna.OrderManagement\Klarna.OrderManagement.csproj -IncludeReferencedProjects
 nuget pack Klarna.OrderManagement.CommerceManager\Klarna.OrderManagement.CommerceManager.csproj -IncludeReferencedProjects
+nuget pack Klarna.Checkout\Klarna.Checkout.csproj -IncludeReferencedProjects
+nuget pack Klarna.Checkout.CommerceManager\Klarna.Checkout.CommerceManager.csproj -IncludeReferencedProjects
 
 copy EPiServer.Klarna.*.nupkg ..\demo\lib /Y
 
