b50d4866a94b793da2adf6b151ad5bdc261c847f
Brian Weeteling <brianweeteling@gmail.com>
2017-05-08 12:06:35 +0200

    Add checkout service with some example code

---
+++ src/Klarna.Checkout/IKlarnaCheckoutService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using EPiServer.Commerce.Order;
+using Klarna.Rest.Models;
+
+namespace Klarna.Checkout
+{
+    public interface IKlarnaCheckoutService
+    {
+        CheckoutOrderData CreateOrUpdateSession(ICart cart);
+        CheckoutOrderData GetOrder(ICart cart);
+        CheckoutOrderData CreateOrder(ICart cart);
+        CheckoutOrderData UpdateOrder(ICart cart);
+    }
+}
---
+++ src/Klarna.Checkout/KlarnaCheckoutService.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using EPiServer;
+using EPiServer.Commerce.Order;
+using EPiServer.ServiceLocation;
+using EPiServer.Logging;
+using EPiServer.Web.Routing;
+using Klarna.Rest;
+using Klarna.Rest.Checkout;
+using Klarna.Rest.Models;
+using Klarna.Rest.Transport;
+using Mediachase.Commerce.Catalog;
+
+namespace Klarna.Checkout
+{
+    [ServiceConfiguration(typeof(IKlarnaCheckoutService))]
+    public class KlarnaCheckoutService : IKlarnaCheckoutService
+    {
+        private readonly ILogger _logger = LogManager.GetLogger(typeof(KlarnaCheckoutService));
+        private readonly IOrderGroupTotalsCalculator _orderGroupTotalsCalculator;
+        
+        private readonly IOrderRepository _orderRepository;
+        private readonly ReferenceConverter _referenceConverter;
+        private readonly UrlResolver _urlResolver;
+        private readonly IContentRepository _contentRepository;
+        private readonly IOrderNumberGenerator _orderNumberGenerator;
+        private readonly IPaymentProcessor _paymentProcessor;
+        private readonly IOrderGroupCalculator _orderGroupCalculator;
+
+        public KlarnaCheckoutService(
+            IOrderGroupTotalsCalculator orderGroupTotalsCalculator,
+            IOrderRepository orderRepository,
+            ReferenceConverter referenceConverter,
+            UrlResolver urlResolver,
+            IContentRepository contentRepository,
+            IOrderNumberGenerator orderNumberGenerator,
+            IPaymentProcessor paymentProcessor,
+            IOrderGroupCalculator orderGroupCalculator)
+        {
+            _orderGroupTotalsCalculator = orderGroupTotalsCalculator;
+            _orderRepository = orderRepository;
+            _referenceConverter = referenceConverter;
+            _urlResolver = urlResolver;
+            _contentRepository = contentRepository;
+            _orderNumberGenerator = orderNumberGenerator;
+            _paymentProcessor = paymentProcessor;
+            _orderGroupCalculator = orderGroupCalculator;
+        }
+
+        public CheckoutOrderData CreateOrUpdateSession(ICart cart)
+        {
+            if (string.IsNullOrWhiteSpace(cart.Properties["test"]?.ToString()))
+            {
+                return CreateOrder(cart);
+            }
+            else
+            {
+                return UpdateOrder(cart);
+            }
+        }
+
+        public CheckoutOrderData GetOrder(ICart cart)
+        {
+            var orderID = "12345";
+            var client = GetClient();
+            var order = client.NewCheckoutOrder(orderID);
+
+            try
+            {
+                var orderData = order.Fetch();
+                return orderData;
+            }
+            catch (ApiException ex)
+            {
+                Console.WriteLine(ex.ErrorMessage.ErrorCode);
+                Console.WriteLine(ex.ErrorMessage.ErrorMessages);
+                Console.WriteLine(ex.ErrorMessage.CorrelationId);
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return null;
+        }
+
+        public CheckoutOrderData CreateOrder(ICart cart)
+        {
+            var client = GetClient();
+            var checkout = client.NewCheckoutOrder();
+
+            var orderLine = new OrderLine
+            {
+                Type = "physical",
+                Reference = "123050",
+                Name = "Tomatoes",
+                Quantity = 10,
+                QuantityUnit = "kg",
+                UnitPrice = 600,
+                TaxRate = 2500,
+                TotalAmount = 6000,
+                TotalTaxAmount = 1200
+            };
+
+            var orderLine2 = new OrderLine
+            {
+                Type = "physical",
+                Reference = "543670",
+                Name = "Bananas",
+                Quantity = 1,
+                QuantityUnit = "bag",
+                UnitPrice = 5000,
+                TaxRate = 2500,
+                TotalAmount = 4000,
+                TotalDiscountAmount = 1000,
+                TotalTaxAmount = 800
+            };
+
+            var merchantUrls = new MerchantUrls
+            {
+                Terms = new Uri("http://www.merchant.com/toc"),
+                Checkout = new Uri("http://www.merchant.com/checkout?klarna_order_id={checkout.order.id}"),
+                Confirmation = new Uri("http://www.merchant.com/thank-you?klarna_order_id={checkout.order.id}"),
+                Push = new Uri("http://www.merchant.com/create_order?klarna_order_id={checkout.order.id}")
+            };
+
+            var orderData = new CheckoutOrderData()
+            {
+                PurchaseCountry = "gb",
+                PurchaseCurrency = "gbp",
+                Locale = "en-gb",
+                OrderAmount = 10000,
+                OrderTaxAmount = 2000,
+                OrderLines = new List<OrderLine> { orderLine, orderLine2 },
+                MerchantUrls = merchantUrls
+            };
+
+            try
+            {
+                checkout.Create(orderData);
+                orderData = checkout.Fetch();
+
+                var orderID = orderData.OrderId;
+                return orderData;
+            }
+            catch (ApiException ex)
+            {
+                Console.WriteLine(ex.ErrorMessage.ErrorCode);
+                Console.WriteLine(ex.ErrorMessage.ErrorMessages);
+                Console.WriteLine(ex.ErrorMessage.CorrelationId);
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return null;
+        }
+
+        public CheckoutOrderData UpdateOrder(ICart cart)
+        {
+            var orderID = "12345";
+            var client = this.GetClient();
+
+            var checkout = client.NewCheckoutOrder(orderID);
+
+            var orderData = new CheckoutOrderData
+            {
+                OrderAmount = 11000,
+                OrderTaxAmount = 2200
+            };
+
+            var lines = new
+                List<OrderLine>
+                {
+                    new OrderLine()
+                    {
+                        Type = "physical",
+                        Reference = "123050",
+                        Name = "Tomatoes",
+                        Quantity = 10,
+                        QuantityUnit = "kg",
+                        UnitPrice = 600,
+                        TaxRate = 2500,
+                        TotalAmount = 6000,
+                        TotalTaxAmount = 1200
+                    },
+                    new OrderLine()
+                    {
+                        Type = "physical",
+                        Reference = "543670",
+                        Name = "Bananas",
+                        Quantity = 1,
+                        QuantityUnit = "bag",
+                        UnitPrice = 5000,
+                        TaxRate = 2500,
+                        TotalAmount = 4000,
+                        TotalDiscountAmount = 1000,
+                        TotalTaxAmount = 800
+                    },
+                    new OrderLine()
+                    {
+                        Type = "shipping_fee",
+                        Name = "Express delivery",
+                        Quantity = 1,
+                        UnitPrice = 1000,
+                        TaxRate = 2500,
+                        TotalAmount = 1000,
+                        TotalTaxAmount = 200
+                    }
+                };
+
+            orderData.OrderLines = lines;
+
+            try
+            {
+                orderData = checkout.Update(orderData);
+                return orderData;
+            }
+            catch (ApiException ex)
+            {
+                Console.WriteLine(ex.ErrorMessage.ErrorCode);
+                Console.WriteLine(ex.ErrorMessage.ErrorMessages);
+                Console.WriteLine(ex.ErrorMessage.CorrelationId);
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return null;
+        }
+        private Client GetClient()
+        {
+            const string MerchantId = "0";
+            const string SharedSecret = "sharedSecret";
+
+            var connector = ConnectorFactory.Create(MerchantId, SharedSecret, Client.EuTestBaseUrl);
+
+            var client = new Client(connector);
+            return client;
+        }
+    }
+}
+
