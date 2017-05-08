5881b98bdfe6f882b0593dd6c966de00c8bfe58f
Brian Weeteling <brianweeteling@gmail.com>
2017-05-08 11:32:02 +0200

    Add vms

---
+++ demo/Sources/EPiServer.Reference.Commerce.Site/Features/Payment/PaymentMethods/KlarnaCheckoutPaymentMethod.cs
@@ -0,0 +1,53 @@
+﻿using EPiServer.Commerce.Order;
+using EPiServer.Framework.Localization;
+using EPiServer.ServiceLocation;
+using Mediachase.Commerce.Orders;
+using System;
+using System.ComponentModel;
+using Klarna.Checkout;
+
+namespace EPiServer.Reference.Commerce.Site.Features.Payment.PaymentMethods
+{
+    public class KlarnaCheckoutPaymentMethod : PaymentMethodBase, IDataErrorInfo
+    {
+        public KlarnaCheckoutPaymentMethod()
+            : this(LocalizationService.Current, ServiceLocator.Current.GetInstance<IOrderGroupFactory>())
+        {
+        }
+
+        public KlarnaCheckoutPaymentMethod(LocalizationService localizationService, IOrderGroupFactory orderGroupFactory)
+            : base(localizationService, orderGroupFactory)
+        {
+        }
+        
+
+        public override IPayment CreatePayment(decimal amount, IOrderGroup orderGroup)
+        {
+            var payment = orderGroup.CreatePayment(_orderGroupFactory);
+            payment.PaymentType = PaymentType.Other;
+            payment.PaymentMethodId = PaymentMethodId;
+            payment.PaymentMethodName = Constants.KlarnaCheckoutSystemKeyword;
+            payment.Amount = amount;
+            payment.Status = PaymentStatus.Pending.ToString();
+            payment.TransactionType = TransactionType.Authorization.ToString();
+            return payment;
+        }
+
+        public override void PostProcess(IPayment payment)
+        {
+           
+        }
+
+        public override bool ValidateData()
+        {
+            return true;
+        }
+
+        public string this[string columnName]
+        {
+            get { return string.Empty; }
+        }
+
+        public string Error { get; }
+    }
+}
---
+++ demo/Sources/EPiServer.Reference.Commerce.Site/Features/Payment/ViewModels/KlarnaCheckoutViewModel.cs
@@ -0,0 +1,21 @@
+﻿using EPiServer.Reference.Commerce.Site.Features.Cart.Services;
+using EPiServer.Reference.Commerce.Site.Features.Payment.PaymentMethods;
+using EPiServer.ServiceLocation;
+
+namespace EPiServer.Reference.Commerce.Site.Features.Payment.ViewModels
+{
+    public class KlarnaCheckoutViewModel : PaymentMethodViewModel<KlarnaCheckoutPaymentMethod>
+    {
+        public Injected<ICartService> InjectedCartService { get; set; }
+        public ICartService CartService => InjectedCartService.Service;
+
+        public KlarnaCheckoutViewModel()
+        {
+            InitializeValues();
+        }
+
+        public void InitializeValues()
+        {
+        }
+    }
+}
