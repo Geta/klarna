fe87d6dbb273b515cee98af5de8d032e29202ed0
Patrick van Kleef <patrick@geta.no>
2017-05-08 10:23:12 +0200

    Add klarna checkout gateway

---
+++ src/Klarna.Checkout/KlarnaCheckoutGateway.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using EPiServer.Commerce.Order;
+using Mediachase.Commerce.Orders;
+using Mediachase.Commerce.Plugins.Payment;
+using EPiServer.Logging;
+
+namespace Klarna.Checkout
+{
+    public class KlarnaCheckoutGateway : AbstractPaymentGateway, IPaymentPlugin
+    {
+        private static readonly ILogger Logger = LogManager.GetLogger(typeof(KlarnaCheckoutGateway));
+        private IOrderForm _orderForm;
+
+        public IOrderGroup OrderGroup { get; set; }
+
+        public override bool ProcessPayment(Payment payment, ref string message)
+        {
+            OrderGroup = payment.Parent.Parent;
+            _orderForm = payment.Parent;
+            return ProcessPayment(payment as IPayment, ref message);
+        }
+
+        public bool ProcessPayment(IPayment payment, ref string message)
+        {
+            try
+            {
+                Logger.Debug("Klarna checkout gateway. Processing Payment ....");
+
+                if (_orderForm == null)
+                {
+                    _orderForm = OrderGroup.Forms.FirstOrDefault(form => form.Payments.Contains(payment));
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("Process checkout failed with error: " + ex.Message, ex);
+                message = ex.Message;
+                throw;
+            }
+        }
+    }
+}
