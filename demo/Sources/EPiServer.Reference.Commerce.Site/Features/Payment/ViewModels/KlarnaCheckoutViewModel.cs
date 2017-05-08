4213b3990d9e96916f9f37426833264480f02311
Patrick van Kleef <patrick@geta.no>
2017-05-08 13:19:25 +0200

    Render html code snippet on klarna checkout view

--- demo/Sources/EPiServer.Reference.Commerce.Site/Features/Payment/ViewModels/KlarnaCheckoutViewModel.cs
+++ demo/Sources/EPiServer.Reference.Commerce.Site/Features/Payment/ViewModels/KlarnaCheckoutViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using EPiServer.Reference.Commerce.Site.Features.Cart.Services;
 using EPiServer.Reference.Commerce.Site.Features.Payment.PaymentMethods;
 using EPiServer.ServiceLocation;
+using Klarna.Checkout;
 
 namespace EPiServer.Reference.Commerce.Site.Features.Payment.ViewModels
 {
@@ -9,13 +10,21 @@ namespace EPiServer.Reference.Commerce.Site.Features.Payment.ViewModels
         public Injected<ICartService> InjectedCartService { get; set; }
         public ICartService CartService => InjectedCartService.Service;
 
+        public Injected<IKlarnaCheckoutService> InjectedKlarnaCheckoutService { get; set; }
+        public IKlarnaCheckoutService KlarnaPaymentsService => InjectedKlarnaCheckoutService.Service;
+
         public KlarnaCheckoutViewModel()
         {
             InitializeValues();
         }
 
+        public string HtmlSnippet { get; set; }
+
         public void InitializeValues()
         {
+            var cart = CartService.LoadCart(CartService.DefaultCartName);
+
+            HtmlSnippet = KlarnaPaymentsService.GetOrder(cart)?.HtmlSnippet;
         }
     }
 }
