1fcfaf7d897858698bbafa138df0a79243a6113f
Brian Weeteling <brianweeteling@gmail.com>
2017-05-23 09:53:59 +0200

    Set some defaults on checkout configuration

--- src/Klarna.Checkout/CheckoutConfiguration.cs
+++ src/Klarna.Checkout/CheckoutConfiguration.cs
@@ -4,19 +4,19 @@ namespace Klarna.Checkout
 {
     public class CheckoutConfiguration : ConnectionConfiguration
     {
-        public bool ShippingOptionsInIFrame { get; set; }
+        public bool ShippingOptionsInIFrame { get; set; } = true;
         public bool AllowSeparateShippingAddress { get; set; }
         public bool DateOfBirthMandatory { get; set; }
         public string ShippingDetailsText { get; set; }
         public bool TitleMandatory { get; set; }
         public bool ShowSubtotalDetail { get; set; }
-        public bool RequireValidateCallbackSuccess { get; set; }
+        public bool RequireValidateCallbackSuccess { get; set; } = true;
         public string AdditionalCheckboxText { get; set; }
         public bool AdditionalCheckboxDefaultChecked { get; set; }
         public bool AdditionalCheckboxRequired { get; set; }
         public bool SendShippingCountries { get; set; }
         public bool PrefillAddress { get; set; }
-        public bool SendShippingOptionsPriorAddresses { get; set; }
+        public bool SendShippingOptionsPriorAddresses { get; set; } = true;
         public string ConfirmationUrl { get; set; }
         public string TermsUrl { get; set; }
         public string CheckoutUrl { get; set; }
