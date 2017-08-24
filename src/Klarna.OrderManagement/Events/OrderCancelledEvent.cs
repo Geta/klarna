7bb74adc91efd614ef8ae241b22ea0025376f44a
Māris Krivtežs <marisks@gmail.com>
2017-08-24 10:22:22 +0300

    Refactoring event handling into separate classes.

---
+++ src/Klarna.OrderManagement/Events/OrderCancelledEvent.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using EPiServer.Commerce.Order;
+using Mediachase.Commerce.Orders;
+
+namespace Klarna.OrderManagement.Events
+{
+    public class OrderCancelledEvent
+    {
+        public OrderCancelledEvent(IPurchaseOrder purchaseOrder)
+        {
+            PurchaseOrder = purchaseOrder ?? throw new ArgumentNullException(nameof(purchaseOrder));
+        }
+
+        public IPurchaseOrder PurchaseOrder { get; }
+    }
+
+    public class OrderCancelledEventHandler
+    {
+        private readonly IPaymentProcessor _paymentProcessor;
+        private readonly IOrderGroupFactory _orderGroupFactory;
+        private IPurchaseOrder _order;
+        private IOrderForm _orderForm;
+
+        public OrderCancelledEventHandler(
+            IPaymentProcessor paymentProcessor,
+            IOrderGroupFactory orderGroupFactory)
+        {
+            _paymentProcessor = paymentProcessor ?? throw new ArgumentNullException(nameof(paymentProcessor));
+            _orderGroupFactory = orderGroupFactory ?? throw new ArgumentNullException(nameof(orderGroupFactory));
+        }
+
+        public void Handle(OrderCancelledEvent ev)
+        {
+            _order = ev.PurchaseOrder;
+            _orderForm = _order.GetFirstForm();
+
+            if (AlreadyVoided()) return;
+
+            var previousPayment = _orderForm.Payments.FirstOrDefault();
+            if (previousPayment == null) return;
+
+            var voidPayment = _order.CreatePayment(_orderGroupFactory);
+            voidPayment.PaymentType = previousPayment.PaymentType;
+            voidPayment.PaymentMethodId = previousPayment.PaymentMethodId;
+            voidPayment.PaymentMethodName = previousPayment.PaymentMethodName;
+            voidPayment.Amount = previousPayment.Amount;
+            voidPayment.Status = PaymentStatus.Pending.ToString();
+            voidPayment.TransactionType = TransactionType.Void.ToString();
+
+            _order.AddPayment(voidPayment);
+
+            _paymentProcessor.ProcessPayment(_order, voidPayment, _order.GetFirstShipment());
+        }
+
+        private bool AlreadyVoided()
+        {
+            return _orderForm.Payments.Any(
+                p => p.TransactionType == TransactionType.Void.ToString()
+                     || p.TransactionType == TransactionType.Capture.ToString());
+        }
+    }
+}
--- src/Klarna.OrderManagement/Initialization/OrderInitialization.cs
+++ src/Klarna.OrderManagement/Initialization/OrderInitialization.cs
@@ -2,8 +2,8 @@
 using EPiServer.Commerce.Order;
 using EPiServer.Framework;
 using EPiServer.Framework.Initialization;
-using EPiServer.Logging;
 using EPiServer.ServiceLocation;
+using Klarna.OrderManagement.Events;
 using Mediachase.Commerce.Orders;
 
 namespace Klarna.OrderManagement.Initialization
@@ -12,72 +12,48 @@ namespace Klarna.OrderManagement.Initialization
     [ModuleDependency(typeof(EPiServer.Commerce.Initialization.InitializationModule))]
     internal class OrderInitialization : IInitializableModule
     {
-        private static readonly ILogger Logger = LogManager.GetLogger(typeof(OrderInitialization));
-        private Injected<IOrderRepository> _orderRepository;
-        private Injected<IOrderGroupFactory> _orderGroupFactory;
-        private Injected<IPaymentProcessor> _paymentProcessor;
+        private IServiceLocator _locator;
 
         public void Initialize(InitializationEngine context)
         {
+            _locator = context.Locate.Advanced;
             OrderContext.Current.OrderGroupUpdated += Current_OrderGroupUpdated;
         }
 
         private void Current_OrderGroupUpdated(object sender, OrderGroupEventArgs e)
         {
-            if (e.OrderGroupType == OrderGroupEventType.PurchaseOrder)
+            var orderRepository = _locator.GetInstance<IOrderRepository>();
+            if (e.OrderGroupType != OrderGroupEventType.PurchaseOrder) return;
+
+            var orderAfterSave = sender as IPurchaseOrder;
+            var orderBeforeSave = orderRepository.Load<IPurchaseOrder>(e.OrderGroupId);
+            if (orderBeforeSave == null || orderAfterSave == null) return;
+
+            // multi shipment scenario. Call payment gateway to release remaining authorization
+            if (IsReleaseRemaining(orderBeforeSave, orderAfterSave))
             {
-                var orderAfterSave = sender as IPurchaseOrder;
-                var orderBeforeSave = _orderRepository.Service.Load<IPurchaseOrder>(e.OrderGroupId);
-                if (orderBeforeSave != null && orderAfterSave != null)
-                {
-                    // multi shipment scenario. Call payment gateway to release remaining authorization
-                    if (orderBeforeSave.OrderStatus == OrderStatus.PartiallyShipped && orderAfterSave.OrderStatus == OrderStatus.Completed && orderAfterSave.GetFirstForm().Shipments.Any(s => s.OrderShipmentStatus == OrderShipmentStatus.Cancelled))
-                    {
-                        if (orderAfterSave.GetFirstForm().Payments.All(p => p.TransactionType != KlarnaAdditionalTransactionType.ReleaseRemainingAuthorization.ToString()))
-                        {
-                            var previousPayment = orderAfterSave.GetFirstForm().Payments.FirstOrDefault();
-                            if (previousPayment != null)
-                            {
-                                var payment = orderAfterSave.CreatePayment(_orderGroupFactory.Service);
-                                payment.PaymentType = previousPayment.PaymentType;
-                                payment.PaymentMethodId = previousPayment.PaymentMethodId;
-                                payment.PaymentMethodName = previousPayment.PaymentMethodName;
-
-                                var remainingAmount = orderAfterSave.GetFirstForm().Shipments.Where(s => s.OrderShipmentStatus != OrderShipmentStatus.Shipped).Sum(x => (x.GetShippingItemsTotal(orderAfterSave.Currency).Amount + x.GetShippingCost(orderAfterSave.Market, orderAfterSave.Currency).Amount));
-
-                                payment.Amount = remainingAmount;
-                                payment.Status = PaymentStatus.Pending.ToString();
-                                payment.TransactionType = KlarnaAdditionalTransactionType.ReleaseRemainingAuthorization.ToString();
-
-                                orderAfterSave.AddPayment(payment);
-
-                                _paymentProcessor.Service.ProcessPayment(orderAfterSave, payment, orderAfterSave.GetFirstShipment());
-                            }
-                        }
-                    }
-                    else if (orderBeforeSave.OrderStatus != OrderStatus.Cancelled && orderAfterSave.OrderStatus == OrderStatus.Cancelled)
-                    { 
-                        if (orderAfterSave.GetFirstForm().Payments.All(p => p.TransactionType != TransactionType.Void.ToString() && p.TransactionType != TransactionType.Capture.ToString()))
-                        {
-                            var previousPayment = orderAfterSave.GetFirstForm().Payments.FirstOrDefault();
-                            if (previousPayment != null)
-                            {
-                                var payment = orderAfterSave.CreatePayment(_orderGroupFactory.Service);
-                                payment.PaymentType = previousPayment.PaymentType;
-                                payment.PaymentMethodId = previousPayment.PaymentMethodId;
-                                payment.PaymentMethodName = previousPayment.PaymentMethodName;
-                                payment.Amount = previousPayment.Amount;
-                                payment.Status = PaymentStatus.Pending.ToString();
-                                payment.TransactionType = TransactionType.Void.ToString();
-
-                                orderAfterSave.AddPayment(payment);
-
-                                _paymentProcessor.Service.ProcessPayment(orderAfterSave, payment, orderAfterSave.GetFirstShipment());
-                            }
-                        }
-                    }
-                }
+                var reseaseRemainingEventHandler = _locator.GetInstance<ReleaseRemainingEventHandler>();
+                reseaseRemainingEventHandler.Handle(new ReleaseRemainingEvent(orderAfterSave));
             }
+            else if (IsCancelled(orderBeforeSave, orderAfterSave))
+            {
+                var orderCancelledEventHandler = _locator.GetInstance<OrderCancelledEventHandler>();
+                orderCancelledEventHandler.Handle(new OrderCancelledEvent(orderAfterSave));
+            }
+        }
+
+        private static bool IsReleaseRemaining(IPurchaseOrder orderBeforeSave, IPurchaseOrder orderAfterSave)
+        {
+            var form = orderAfterSave.GetFirstForm();
+            return orderBeforeSave.OrderStatus == OrderStatus.PartiallyShipped
+                   && orderAfterSave.OrderStatus == OrderStatus.Completed
+                   && form.Shipments.Any(s => s.OrderShipmentStatus == OrderShipmentStatus.Cancelled)
+                   && form.Payments.All(p => p.TransactionType != KlarnaAdditionalTransactionType.ReleaseRemainingAuthorization.ToString());
+        }
+
+        private static bool IsCancelled(IPurchaseOrder orderBeforeSave, IPurchaseOrder orderAfterSave)
+        {
+            return orderBeforeSave.OrderStatus != OrderStatus.Cancelled && orderAfterSave.OrderStatus == OrderStatus.Cancelled;
         }
 
         public void Uninitialize(InitializationEngine context)
