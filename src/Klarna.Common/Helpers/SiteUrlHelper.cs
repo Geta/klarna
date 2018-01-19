ac9c5b804ab068614b480e56407bcaea90931be1
Patrick van Kleef <patrick@geta.no>
2018-01-19 14:21:28 +0100

    KC-14 Enhancement - product_url, image_url, & product_identifiers

--- src/Klarna.Common/Helpers/SiteUrlHelper.cs
+++ src/Klarna.Common/Helpers/SiteUrlHelper.cs
@@ -1,19 +1,38 @@
-﻿using EPiServer.Web;
+﻿using System.Linq;
+using EPiServer.ServiceLocation;
+using EPiServer.Web;
 
 namespace Klarna.Common.Helpers
 {
     public static class SiteUrlHelper
     {
+        private static Injected<ISiteDefinitionRepository> SiteDefinitionRepository { get; set; }
+
         public static string GetAbsoluteUrl()
         {
             var siteDefinition = SiteDefinition.Current;
-            if (siteDefinition != null)
+            if (siteDefinition != null && siteDefinition.SiteUrl != null)
             {
-                var siteUrl = siteDefinition.SiteUrl.ToString();
-                
-                return siteUrl.Substring(0, siteUrl.LastIndexOf("/") !=1 ? siteUrl.LastIndexOf("/") : (siteUrl.Length - 1));
+                return GetUrl(siteDefinition.SiteUrl.ToString());
+            }
+            else
+            {
+                var firstSite = SiteDefinitionRepository.Service.List().FirstOrDefault();
+                if (firstSite != null && firstSite.SiteUrl != null)
+                {
+                    return GetUrl(firstSite.SiteUrl.ToString());
+                }
             }
             return string.Empty;
+        }
+
+        private static string GetUrl(string siteUrl)
+        {
+            if (string.IsNullOrEmpty(siteUrl))
+            {
+                return string.Empty;
+            }
+            return siteUrl.Substring(0, siteUrl.LastIndexOf("/") != 1 ? siteUrl.LastIndexOf("/") : (siteUrl.Length - 1));
         }
     }
 }
