65734dcea12261a07ac6d4757ea74fc723eb5723
Māris Krivtežs <marisks@gmail.com>
2019-10-16 13:55:30 +0300

    Allow plus in an email.

--- demo/Sources/EPiServer.Reference.Commerce.Site/Infrastructure/Attributes/LocalizedEmailAttribute.cs
+++ demo/Sources/EPiServer.Reference.Commerce.Site/Infrastructure/Attributes/LocalizedEmailAttribute.cs
@@ -3,7 +3,7 @@
     public class LocalizedEmailAttribute : LocalizedRegularExpressionAttribute
     {
         public LocalizedEmailAttribute(string name)
-            : base(@"^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$", name)
+            : base(@"^([0-9a-zA-Z]([-.+\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$", name)
         {
         }
     }
