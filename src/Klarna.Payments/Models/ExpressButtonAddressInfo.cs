3b287199cabdfc3ab4b15733ed5e1878f8930cff
Frederik Vig <frederik@getadigital.com>
2022-02-28 06:36:53 -0800

    Started on express button

---
+++ src/Klarna.Payments/Models/ExpressButtonAddressInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+using Klarna.Common.Models;
+
+namespace Klarna.Payments.Models
+{
+    public class ExpressButtonAddressInfo : OrderManagementAddressInfo
+    {
+        /// <summary>
+        /// Since Express Button uses first_name and the rest given_name we make this private and set the GivenName property with the value.
+        /// </summary>
+        [JsonPropertyName("first_name")]
+        private string FirstName { set => GivenName = value; }
+
+        /// <summary>
+        /// Since Express Button uses last_name and the rest family_name we make this private and set the FamilyName property with the value.
+        /// </summary>
+        [JsonPropertyName("last_name")]
+        private string LastName { set => FamilyName = value; }
+
+        /// <summary>
+        /// ISO 8601 date. The customer date of birth
+        /// </summary>
+        [JsonPropertyName("date_of_birth")]
+        public string DateOfBirth { get; set; }
+    }
+}
